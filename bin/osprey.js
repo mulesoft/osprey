23e42fa8081b0aa8135c4496c762c891caefe30f
Blake Embrey <hello@blakeembrey.com>
2015-03-18 17:02:48 -0300

    update ambiguous -a cli option

--- bin/osprey.js
+++ bin/osprey.js
@@ -6,10 +6,10 @@ var parser = require('raml-parser');
 var argv = require('yargs')
     .usage(
       'Generate an API proxy from a RAML definition.\n\n' +
-      'Usage: $0 -f [file] -a [endpoint] -p [port number]'
+      'Usage: $0 -f [file] -a [address] -p [port number]'
     )
     .demand(['a', 'f', 'p'])
-    .describe('a', 'Proxy address')
+    .describe('a', 'Proxy endpoint address')
     .describe('f', 'Path to the RAML definition')
     .describe('p', 'Port number to bind the proxy')
     .describe('docs', 'Serve documentation from a path')
