2bd36b34a4b6f0186d16d6c4595926df5cd17b26
Blake Embrey <hello@blakeembrey.com>
2015-08-17 18:56:14 -0700

    Add a simple example with Express

---
+++ examples/express/index.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var osprey = require('../..')
+var join = require('path').join
+
+var PORT = process.env.PORT || 3000
+
+var router = osprey.Router()
+
+router.get('/users', function (req, res) {
+  res.json([
+    {
+      username: 'blakeembrey',
+      password: 'hunter2'
+    }
+  ])
+})
+
+osprey.loadFile(join(__dirname, 'api.raml'))
+  .then(function (middleware) {
+    var app = express()
+
+    app.use('/v1', middleware)
+    app.use('/v1', router)
+
+    app.listen(PORT, function () {
+      console.log('Application listening on ' + PORT + '...')
+    })
+  })
