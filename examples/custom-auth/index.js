9df60c9a4f80d96471a414bfaa2f5cda24b85f54
Blake Embrey <hello@blakeembrey.com>
2015-08-17 18:43:38 -0700

    Add an example using custom authentication

---
+++ examples/custom-auth/index.js
@@ -0,0 +1,46 @@
+var http = require('http')
+var osprey = require('../..')
+var finalhandler = require('finalhandler')
+var join = require('path').join
+
+var PORT = process.env.PORT || 3000
+
+// Create a custom authentication handler to force users to give me a token.
+function handler () {
+  return function (req, res, next) {
+    if (!req.headers.token) {
+      return next(new Error('No token'))
+    }
+
+    // Check with the database and see if the token is valid.
+    setTimeout(function () {
+      return next()
+    }, 1000)
+  }
+}
+
+osprey.loadFile(join(__dirname, 'api.raml'), {
+  security: {
+    token_auth: function () {
+      return { handler: handler }
+    }
+  }
+})
+  .then(function (middleware) {
+    var router = osprey.Router()
+
+    router.use(middleware)
+
+    router.get('/posts', function (req, res) {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end('[]')
+    })
+
+    var app = http.createServer(function (req, res) {
+      router(req, res, finalhandler(req, res))
+    })
+
+    app.listen(PORT, function () {
+      console.log('Application listening on ' + PORT + '...')
+    })
+  })
