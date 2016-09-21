14e5112f5339bc026b24a584cb7ad49a482144bb
Jonathan Stoikovitch <jstoiko@users.noreply.github.com>
2016-09-20 21:52:17 -0700

    adding initial (temporary) RAML 1.0 tests

---
+++ test/proxy1.js
@@ -0,0 +1,265 @@
+/* global describe, before, after, it */
+
+var expect = require('chai').expect
+var popsicle = require('popsicle')
+var router = require('osprey-router')
+var join = require('path').join
+var bodyParser = require('body-parser')
+var serverAddress = require('server-address')
+var Busboy = require('busboy')
+var parser = require('raml-1-parser')
+var osprey = require('../')
+var utils = require('./support/utils')
+
+var EXAMPLE_RAML_PATH = join(__dirname, 'fixtures/example1.raml')
+
+var success = utils.response('success')
+
+describe('proxy (RAML 1.0)', function () {
+  var app
+  var proxy
+  var server
+
+  before(function () {
+    app = router()
+    server = serverAddress(utils.createServer(app))
+
+    server.listen()
+
+    return parser.loadRAML(EXAMPLE_RAML_PATH)
+      .then(function (ramlApi) {
+        var raml = ramlApi.toJSON({
+          serializeMetadata: false
+        })
+        var ospreyApp = osprey.server(raml)
+        var proxyApp = osprey.proxy(ospreyApp, server.url())
+
+        proxy = serverAddress(proxyApp)
+        proxy.listen()
+      })
+  })
+
+  after(function () {
+    proxy.close()
+    server.close()
+  })
+
+  describe('routes', function () {
+    it('should proxy defined routes', function () {
+      app.get('/users', success)
+
+      return popsicle.default(proxy.url('/users'))
+        .then(function (res) {
+          expect(res.body).to.equal('success')
+          expect(res.status).to.equal(200)
+        })
+    })
+
+    it('should block undefined routes', function () {
+      app.get('/unknown', success)
+
+      return popsicle.default(proxy.url('/unknown'))
+        .then(function (res) {
+          expect(res.status).to.equal(404)
+        })
+    })
+  })
+
+  describe('query parameters', function () {
+    it('should filter unknown query parameters', function () {
+      app.get('/query', function (req, res, next) {
+        expect(req.url).to.equal('/query?hello=world')
+
+        return next()
+      }, success)
+
+      return popsicle.default(proxy.url('/query?hello=world&test=true'))
+        .then(function (res) {
+          expect(res.body).to.equal('success')
+          expect(res.status).to.equal(200)
+        })
+    })
+
+    it('should reject invalid query parameters', function () {
+      return popsicle.default(proxy.url('/query?hello=12345'))
+        .then(function (res) {
+          expect(res.status).to.equal(400)
+        })
+    })
+  })
+
+  describe('body', function () {
+    describe('json', function () {
+      it('should proxy valid json', function () {
+        app.post(
+          '/json',
+          bodyParser.json(),
+          function middleware (req, res, next) {
+            expect(req.body).to.deep.equal({ hello: 'world' })
+
+            return next()
+          },
+          success
+        )
+
+        return popsicle.default({
+          url: proxy.url('/json'),
+          method: 'post',
+          body: {
+            hello: 'world'
+          }
+        })
+          .then(function (res) {
+            expect(res.body).to.equal('success')
+            expect(res.status).to.equal(200)
+          })
+      })
+
+      it('should reject invalid json', function () {
+        var run = false
+
+        app.post('/json', function (req, res, next) {
+          run = true
+
+          return next()
+        }, success)
+
+        return popsicle.default({
+          url: proxy.url('/json'),
+          method: 'post',
+          body: {
+            hello: 12345
+          }
+        })
+          .then(function (res) {
+            expect(run).to.be.false
+            expect(res.status).to.equal(400)
+          })
+      })
+    })
+
+    describe('urlencoded', function () {
+      it('should proxy valid urlencoded strings', function () {
+        app.post(
+          '/urlencoded',
+          bodyParser.urlencoded({ extended: false }),
+          function middleware (req, res, next) {
+            expect(req.body).to.deep.equal({ hello: 'world' })
+
+            return next()
+          },
+          success
+        )
+
+        return popsicle.default({
+          url: proxy.url('/urlencoded'),
+          method: 'post',
+          body: {
+            hello: 'world'
+          },
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+          }
+        })
+          .then(function (res) {
+            expect(res.body).to.equal('success')
+            expect(res.status).to.equal(200)
+          })
+      })
+
+      it('should reject invalid urlencoded string values', function () {
+        var run = false
+
+        app.post('/urlencoded', function (req, res, next) {
+          run = true
+
+          return next()
+        }, success)
+
+        return popsicle.default({
+          url: proxy.url('/urlencoded'),
+          method: 'post',
+          body: {
+            hello: 12345
+          },
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+          }
+        })
+          .then(function (res) {
+            expect(run).to.be.false
+            expect(res.status).to.equal(400)
+          })
+      })
+    })
+
+    describe('form data', function () {
+      it('should proxy valid form data', function () {
+        app.post(
+          '/formdata',
+          function middleware (req, res, next) {
+            var busboy = new Busboy({ headers: req.headers })
+
+            var fieldname
+            var fieldvalue
+
+            busboy.on('field', function (name, value) {
+              fieldname = name
+              fieldvalue = value
+            })
+
+            busboy.on('finish', function () {
+              expect(fieldname).to.equal('hello')
+              expect(fieldvalue).to.equal('world')
+
+              return next()
+            })
+
+            return req.pipe(busboy)
+          },
+          success
+        )
+
+        return popsicle.default({
+          url: proxy.url('/formdata'),
+          method: 'post',
+          body: {
+            hello: 'world'
+          },
+          headers: {
+            'content-type': 'multipart/form-data'
+          }
+        })
+          .then(function (res) {
+            expect(res.body).to.equal('success')
+            expect(res.status).to.equal(200)
+          })
+      })
+
+      it('should reject invalid form data', function () {
+        var run = false
+
+        app.post('/formdata', function (req, res, next) {
+          run = true
+
+          return next()
+        }, success)
+
+        return popsicle.default({
+          url: proxy.url('/formdata'),
+          method: 'post',
+          body: {
+            hello: 12345
+          },
+          headers: {
+            'content-type': 'multipart/form-data'
+          }
+        })
+          .then(function (res) {
+            expect(run).to.be.false
+            expect(res.status).to.equal(400)
+          })
+      })
+    })
+  })
+})
---
+++ test/server1.js
@@ -0,0 +1,128 @@
+/* global describe, beforeEach, it */
+
+var expect = require('chai').expect
+var popsicle = require('popsicle')
+var server = require('popsicle-server')
+var router = require('osprey-router')
+var join = require('path').join
+var osprey = require('../')
+var utils = require('./support/utils')
+
+var EXAMPLE_RAML_PATH = join(__dirname, 'fixtures/example1.raml')
+
+var success = utils.response('success')
+
+describe('server (RAML 1.0)', function () {
+  var http
+
+  describe('normal usage', function () {
+    beforeEach(function () {
+      return osprey.loadFile(EXAMPLE_RAML_PATH, { server: { cors: true, compression: true } })
+        .then(function (middleware) {
+          var app = router()
+
+          app.use(middleware)
+          expect(middleware.ramlUriParameters).to.deep.equal({
+            userId: {
+              type: ['number'],
+              displayName: 'userId',
+              name: 'userId',
+              required: true
+            }
+          })
+
+          app.get('/users', success)
+          app.get('/unknown', success)
+
+          http = utils.createServer(app)
+        })
+    })
+
+    it('should accept defined routes', function () {
+      return popsicle.default('/users')
+        .use(server(http))
+        .then(function (res) {
+          expect(res.body).to.equal('success')
+          expect(res.status).to.equal(200)
+        })
+    })
+
+    it('should reject undefined routes', function () {
+      return popsicle.default('/unknown')
+        .use(server(http))
+        .then(function (res) {
+          expect(res.status).to.equal(404)
+        })
+    })
+
+    it('should have cors enabled', function () {
+      return popsicle.default({ url: '/users', method: 'options' })
+        .use(server(http))
+        .then(function (res) {
+          expect(res.status).to.equal(204)
+          expect(res.headers['access-control-allow-origin']).to.equal('*')
+          expect(res.headers['access-control-allow-methods']).to.equal('GET,HEAD,PUT,PATCH,POST,DELETE')
+        })
+    })
+
+    it('should have compression enabled', function () {
+      return popsicle.default('/users')
+        .use(server(http))
+        .then(function (res) {
+          expect(res.body).to.equal('success')
+          expect(res.headers['content-encoding']).to.equal('gzip')
+        })
+    })
+  })
+
+  describe('not found handler', function () {
+    beforeEach(function () {
+      return osprey.loadFile(EXAMPLE_RAML_PATH, { server: { notFoundHandler: false } })
+        .then(function (middleware) {
+          var app = router()
+
+          app.use(middleware)
+
+          app.get('/definitelynotfound', success)
+
+          http = utils.createServer(app)
+        })
+    })
+
+    it('should accept defined routes', function () {
+      return popsicle.default('/definitelynotfound')
+        .use(server(http))
+        .then(function (res) {
+          expect(res.body).to.equal('success')
+          expect(res.status).to.equal(200)
+        })
+    })
+  })
+
+  describe('method handler options', function () {
+    beforeEach(function () {
+      return osprey.loadFile(EXAMPLE_RAML_PATH, { server: { discardUnknownQueryParameters: false } })
+        .then(function (middleware) {
+          var app = router()
+
+          app.use(middleware)
+
+          app.get('/users', function (req, res) {
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify(req.url))
+          })
+
+          http = utils.createServer(app)
+        })
+    })
+
+    it('should accept server options in the method handler', function () {
+      return popsicle.get('/users?x=1&y=2')
+        .use(server(http))
+        .then(function (res) {
+          expect(res.body).to.equal('/users?x=1&y=2')
+          expect(res.status).to.equal(200)
+        })
+    })
+  })
+})
